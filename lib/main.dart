259b0d4ef87220829dd5e1b9a68472a1a1226cd0
nevooronni <nevooronni@gmail.com>
2019-09-27 02:13:48 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_widget/category/category_route.dart';
+
+void main() => runApp(UnitConverterApp());
+
+class UnitConverterApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
+    );
+  }
+}
